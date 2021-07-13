df55952db17335cc740a13c17d630b7d91efe027
Conor Grogan <crgrogan@hotmail.com>
2021-07-13 19:23:15 +0100

    Configure webpack.dev.js

---
+++ webpack.dev.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/pages/home/main.js",
+    details: "./src/pages/details/main.js",
+  },
+
+  devServer: {
+    port: 8080,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/pages/home/index.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/details/index.html",
+      inject: true,
+      chunks: ["details"],
+      filename: "details.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
