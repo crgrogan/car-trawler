cc13ebb1e6ec0c1d6c478cb03704b4e13febe356
Conor Grogan <crgrogan@hotmail.com>
2021-07-15 22:52:43 +0100

    Add details page

--- src/components/card/main.js
+++ src/components/card/main.js
@@ -11,26 +11,32 @@ class Card extends HTMLElement {
         </style>
         <article class="card">
             <div class="col col-left">
-              <img class="card-image" src="" alt="Image of car" />
+              <img class="car-image" src="" alt="Image of car" />
             </div>
             <div class="col col-middle">
               <h2 class="car-model"></h2>
-              <h4 class="car-vendor"></h4>
-              <div class="car-info">
-                <span class="car-availability"></span>
-                <span class="car-transmission"></span>
-                <span class="car-fuel"></span>
+              <div class="vendor-details">
+                <span class="car-vendor"></span>
+                <span class="car-code"></span>
               </div>
               <div class="car-info">
                 <span class="car-passenger-qty"></span>
                 <span class="car-baggage-qty"></span>
                 <span class="car-door-count"></span>
               </div>
+              <div class="car-info">
+                <span class="car-air-con"></span>
+                <span class="car-transmission"></span>
+                <span class="car-fuel"></span>
+              </div>
+              <div class="car-info">
+                <span class="car-availability"></span>
+              </div>
             </div>
             <div class="col col-right">
                 <h3 class="car-price-heading">Total Price</h3>
                 <span class="car-price"></span>
-                <a href="/details" class="btn details-btn">View Details</a>
+                <a href="" class="btn details-btn">View Details</a>
             </div>
         </article>`;
 
@@ -49,28 +55,36 @@ class Card extends HTMLElement {
   }
 
   updateUI() {
-    const image = this.shadowRoot.querySelector(".card-image");
+    const image = this.shadowRoot.querySelector(".car-image");
     const model = this.shadowRoot.querySelector(".car-model");
     const vendor = this.shadowRoot.querySelector(".car-vendor");
-    const vehicleAvailability =
-      this.shadowRoot.querySelector(".car-availability");
+    const airCon = this.shadowRoot.querySelector(".car-air-con");
     const transmission = this.shadowRoot.querySelector(".car-transmission");
     const fuelType = this.shadowRoot.querySelector(".car-fuel");
     const passengerQty = this.shadowRoot.querySelector(".car-passenger-qty");
     const baggageQty = this.shadowRoot.querySelector(".car-baggage-qty");
     const doorCount = this.shadowRoot.querySelector(".car-door-count");
     const price = this.shadowRoot.querySelector(".car-price");
+    const vehicleCode = this.shadowRoot.querySelector(".car-code");
+    const vehicleAvailability =
+      this.shadowRoot.querySelector(".car-availability");
+    const detailsBtn = this.shadowRoot.querySelector(".details-btn");
 
+    detailsBtn.href = `/details.html?vendor=${this._carData.vendor}&vehicle=${this._carData.Vehicle["@Code"]}`;
     image.src = this._carData.Vehicle.PictureURL;
     model.textContent = this._carData.Vehicle.VehMakeModel["@Name"];
     vendor.textContent = `Vendor: ${this._carData.vendor}`;
+    airCon.textContent = this._carData.Vehicle["@AirConditionInd"]
+      ? "Air Con"
+      : "";
     fuelType.textContent = this._carData.Vehicle["@FuelType"];
     transmission.textContent = this._carData.Vehicle["@TransmissionType"];
-    vehicleAvailability.textContent = this._carData["@Status"];
     passengerQty.textContent = `${this._carData.Vehicle["@PassengerQuantity"]} People`;
     baggageQty.textContent = `${this._carData.Vehicle["@BaggageQuantity"]} Bags`;
     doorCount.textContent = `${this._carData.Vehicle["@DoorCount"]} Doors`;
-    price.innerHTML = `${this._carData.TotalCharge["@CurrencyCode"]} <sup>$</sup>${this._carData.TotalCharge["@RateTotalAmount"]} `;
+    price.innerHTML = `${this._carData.TotalCharge["@CurrencyCode"]} $${this._carData.TotalCharge["@RateTotalAmount"]} `;
+    vehicleCode.textContent = `Code: ${this._carData.Vehicle["@Code"]}`;
+    vehicleAvailability.innerHTML = `${this._carData["@Status"]}<div class="available-icon"></div>`;
   }
 }
 
--- src/pages/details/main.js
+++ src/pages/details/main.js
@@ -1,2 +1,62 @@
 import("../../css/styles.scss");
 import("./page.scss");
+import data from "../../data/cars.json";
+import "../../components/card/main";
+
+const details = (function () {
+  const init = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const vendorCode = urlParams.get("vendor");
+    const vehicleCode = urlParams.get("vehicle");
+    const vendorsArray = data[0].VehAvailRSCore.VehVendorAvails;
+    const selectedVendor = vendorsArray.filter(
+      (vendor) => vendor.Vendor["@Name"] === vendorCode
+    );
+    const selectedVehicle = selectedVendor[0].VehAvails.filter(
+      (vehicle) => vehicle.Vehicle["@Code"] === vehicleCode
+    );
+    renderData(selectedVendor[0].Vendor, selectedVehicle[0]);
+  };
+
+  const renderData = (vendor, vehicle) => {
+    const carDetailsImage = document.querySelector(".car-details-image");
+    const model = document.querySelector(".car-details-model");
+    const vehicleVendorName = document.querySelector(
+      ".car-details-vendor-name"
+    );
+    const vehicleVendorCode = document.querySelector(
+      ".car-details-vendor-code"
+    );
+    const airCon = document.querySelector(".car-details-air-con");
+    const transmission = document.querySelector(".car-details-transmission");
+    const fuelType = document.querySelector(".car-details-fuel");
+    const passengerQty = document.querySelector(".car-details-passenger-qty");
+    const baggageQty = document.querySelector(".car-details-baggage-qty");
+    const doorCount = document.querySelector(".car-details-door-count");
+    const price = document.querySelector(".car-details-price");
+    const vehicleCode = document.querySelector(".car-details-code");
+    const vehicleAvailability = document.querySelector(
+      ".car-details-availability"
+    );
+
+    carDetailsImage.src = vehicle.Vehicle.PictureURL;
+    model.textContent = vehicle.Vehicle.VehMakeModel["@Name"];
+    vehicleVendorName.innerHTML = `<span class="bold">Name:</span>${vendor["@Name"]}`;
+    vehicleVendorCode.innerHTML = `<span class="bold">Code:</span>${vendor["@Code"]}`;
+    airCon.textContent = vehicle.Vehicle["@AirConditionInd"] ? "Air Con" : "";
+    fuelType.textContent = vehicle.Vehicle["@FuelType"];
+    transmission.textContent = vehicle.Vehicle["@TransmissionType"];
+    passengerQty.textContent = `${vehicle.Vehicle["@PassengerQuantity"]} People`;
+    baggageQty.textContent = `${vehicle.Vehicle["@BaggageQuantity"]} Bags`;
+    doorCount.textContent = `${vehicle.Vehicle["@DoorCount"]} Doors`;
+    price.innerHTML = `${vehicle.TotalCharge["@CurrencyCode"]} $${vehicle.TotalCharge["@RateTotalAmount"]} `;
+    vehicleCode.innerHTML = `<span class="bold">Vehicle Code:</span>${vehicle.Vehicle["@Code"]}`;
+    vehicleAvailability.innerHTML = `${vehicle["@Status"]}<div class="available-icon"></div>`;
+  };
+
+  return { init };
+})();
+
+document.addEventListener("DOMContentLoaded", () => {
+  details.init();
+});
